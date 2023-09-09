0ed0f80d37dacbc5f894ba213f231a9fccad354f
alfonsop123 <109006708+alfonsop123@users.noreply.github.com>
2023-09-08 20:43:01 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Game Blocker Extension</title>
-    <script src="popup.js"></script>
-</head>
-<body>
-    <h2>Game Blocker Extension</h2>
-    <button id="block-button">Block Games</button>
-</body>
-</html>
+document.addEventListener('DOMContentLoaded', function () {
+    const blockButton = document.getElementById('block-button');
+
+    blockButton.addEventListener('click', function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                function: blockGamesOnWebsite,
+            });
+        });
+    });
+
+    function blockGamesOnWebsite() {
+        // Content script to block games on websites
+        const blockedGames = JSON.parse(localStorage.getItem('blockedGames')) || [];
+        const blockedGamesSelector = blockedGames.map(game => `:contains('${game}')`).join(',');
+        const elementsToBlock = document.querySelectorAll(blockedGamesSelector);
+
+        elementsToBlock.forEach(element => {
+            element.style.display = 'none';
+        });
+    }
+});
