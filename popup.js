6d864114fc9cace03f2acfd90fc10c35d0c8ce6d
alfonsop123 <109006708+alfonsop123@users.noreply.github.com>
2023-09-08 21:10:46 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,19 +1,39 @@
 // popup.js
 
-const gameInput = document.getElementById('game-input');
-const addGameBtn = document.getElementById('add-game');
 const websiteInput = document.getElementById('website-input');
 const addWebsiteBtn = document.getElementById('add-website');
 
-// ... (Existing code for blocking games)
+// Function to add a website to the blocked websites list
+function addWebsiteToList(website) {
+    chrome.storage.sync.get(['blockedWebsites'], function (result) {
+        let blockedWebsites = result.blockedWebsites || [];
 
-// Add a website to the blocked list
+        // Normalize the input to remove "https://" if present
+        const normalizedWebsite = website.replace(/^https?:\/\//, '');
+
+        if (normalizedWebsite !== '' && !blockedWebsites.includes(normalizedWebsite)) {
+            blockedWebsites.push(normalizedWebsite);
+            chrome.storage.sync.set({ 'blockedWebsites': blockedWebsites });
+            websiteInput.value = '';
+            // Add logic to update the blocked websites list on your website if needed
+        }
+    });
+}
+
+// Add a website to the blocked list when the button is clicked
 addWebsiteBtn.addEventListener('click', function () {
     const website = websiteInput.value.trim();
-    if (website !== '' && !blockedWebsites.includes(website)) {
-        blockedWebsites.push(website);
-        chrome.storage.sync.set({ 'blockedWebsites': blockedWebsites });
-        websiteInput.value = '';
-        // Add logic to update the blocked websites list on your website if needed
+    if (website !== '') {
+        addWebsiteToList(website);
+    }
+});
+
+// Handle Enter key press in the input field
+websiteInput.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        const website = websiteInput.value.trim();
+        if (website !== '') {
+            addWebsiteToList(website);
+        }
     }
 });
