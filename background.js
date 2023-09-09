58292da5066e88c6c53126e9a4f1b28ccc55a9fe
alfonsop123 <109006708+alfonsop123@users.noreply.github.com>
2023-09-08 20:46:02 -0500

    Create background.js

---
+++ background.js
@@ -0,0 +1,9 @@
+// background.js
+
+// This is an example of a background script for your extension.
+// In this simple example, there are no specific background tasks required.
+
+// You can leave this file empty or add any background-related functionality
+// if your extension needs it in the future.
+
+console.log('Game Blocker Extension background script loaded.');
