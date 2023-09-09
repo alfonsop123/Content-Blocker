26e5962c9922289a093596b47e9221c5b61ef77d
alfonsop123 <109006708+alfonsop123@users.noreply.github.com>
2023-09-08 20:52:26 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 // background.js
 
-// This is an example of a background script for your extension.
-// In this simple example, there are no specific background tasks required.
+// Initialize blockedGames and blockedWebsites as empty arrays
+chrome.storage.sync.get(['blockedGames', 'blockedWebsites'], function(result) {
+    let blockedGames = result.blockedGames || [];
+    let blockedWebsites = result.blockedWebsites || [];
 
-// You can leave this file empty or add any background-related functionality
-// if your extension needs it in the future.
-
-console.log('Game Blocker Extension background script loaded.');
+    // Add or remove your blocked games and websites here
+});
