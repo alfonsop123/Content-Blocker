5464c7cff89fec7ab763e50b1487f0ceac338d05
alfonsop123 <109006708+alfonsop123@users.noreply.github.com>
2023-09-08 20:42:35 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,11 @@
-console.log('This is a popup!');
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Game Blocker Extension</title>
+    <script src="popup.js"></script>
+</head>
+<body>
+    <h2>Game Blocker Extension</h2>
+    <button id="block-button">Block Games</button>
+</body>
+</html>
